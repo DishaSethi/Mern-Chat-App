2661711246dbc68ba670bcd2b9e937523283fa6f
Disha <sethidisha123@gmail.com>
2024-11-21 23:28:33 +0530

    User routes, model, routes, controllers,middlewares, all created and .env file all the secret keys added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,22 @@
 // const express=require("express");
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 const app=express();
+
+const PORT=process.env.PORT
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
+app.listen(PORT,()=>{
+    console.log("Server is running on port :"+PORT);
+    connectDB();
 });
